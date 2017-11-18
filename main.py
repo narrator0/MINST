ccb9f534ae66e679980fca4656b60795890a91cb
arfullight <tmimvhuang@gmail.com>
2017-11-18 12:53:41 -0800

    use desicioin tree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn import svm
+from sklearn import tree
 import numpy as np
 
 from data_process import process_train_data
@@ -7,15 +7,16 @@ from data_process import process_train_data
 train_data = pd.read_csv("data/train.csv")
 features_train, labels_train, features_test, labels_test = process_train_data(train_data)
 
-first_row = features_train.loc[0].values.reshape((28,28))
+# first_row = features_train.loc[0].values.reshape((28,28))
 
 # print np.array2string(first_row, max_line_width=np.inf)
 # print features_test
 
 print "start training"
-clf = svm.SVC()
+clf = tree.DecisionTreeClassifier()
 clf.fit(features_train, labels_train)
 
-print clf.predict(features_test[0:1])
-print labels_test[0:1]
+print [clf.predict(features_test[0:1]), labels_test[0:1]]
+
+print "start calculate score"
 print clf.score(features_test, labels_test)
