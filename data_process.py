efced89276c235d05d84dc76ebccf1452dfe8c35
arfullight <tmimvhuang@gmail.com>
2017-11-18 12:53:37 -0800

    use 10% data as test data

--- data_process.py
+++ data_process.py
@@ -2,4 +2,4 @@ def process_train_data(data):
   labels = data['label']
   features = data.drop('label', axis=1)
 
-  return features[0:10000], labels[0:10000], features[10000:42000], labels[10000:42000]
+  return features[0:37800], labels[0:37800], features[37801:42000], labels[37801:42000]
